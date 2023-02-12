4507a397a2badf8e11c2a4d7b93f62e974a10b9c
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2023-02-12 11:21:21 +0900

    230212

---
+++ problem/p1_two_sum.py
@@ -0,0 +1,12 @@
+﻿# https://leetcode.com/problems/two-sum/solutions/127810/two-sum/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if target == nums[i] + nums[j]:
+                    return [i, j]
+
---
+++ test/p1_test.py
@@ -0,0 +1,38 @@
+﻿import unittest
+from problem.p1_two_sum import Solution
+
+
+class MyTestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = {
+            'nums': [2, 7, 11, 15],
+            'target': 9
+        }
+
+        output_data = [0, 1]
+
+        self.assertEqual(output_data, Solution().twoSum(**input_data))
+
+    def test_example2(self):
+        input_data = {
+            'nums': [3, 2, 4],
+            'target': 6
+        }
+
+        output_data = [1, 2]
+
+        self.assertEqual(output_data, Solution().twoSum(**input_data))
+
+    def test_example3(self):
+        input_data = {
+            'nums': [3, 3],
+            'target': 6
+        }
+
+        output_data = [0, 1]
+
+        self.assertEqual(output_data, Solution().twoSum(**input_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
