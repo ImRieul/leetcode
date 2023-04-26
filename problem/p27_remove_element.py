f52c1930a5bc48fc544b86e8fe555eba07aa9d9b
ImRieul <82865792+ImRieul@users.noreply.github.com>
2023-04-27 07:31:22 +0900

    230427

--- problem/p27_remove_element.py
+++ problem/p27_remove_element.py
@@ -14,7 +14,7 @@ class Solution:
 
 
 
-# 참고(속도를 높이는 방법)
+# 신박한 풀이 (속도를 높이는 방법)
 # class Solution:
 #     def removeElement(self, nums: List[int], val: int) -> int:
 #         while val in nums:
---
+++ problem/p35_search_insert_position.py
@@ -0,0 +1,23 @@
+﻿
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target not in nums:
+            nums.append(target)
+            nums.sort()
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+
+
+# 신박한 풀이
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         is_found = False
+#
+#         for index, item in enumerate(nums):
+#             if item >= target:
+#                 return index
+#
+#         return len(nums)
