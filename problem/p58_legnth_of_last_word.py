ee6d6aae184401783490a7ab8220ede34fcded89
ImRieul <82865792+ImRieul@users.noreply.github.com>
2023-04-28 07:36:38 +0900

    230428

---
+++ problem/p58_legnth_of_last_word.py
@@ -0,0 +1,5 @@
+﻿import re
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(re.findall(r'\w+', s)[-1])
---
+++ test/p58.py
@@ -0,0 +1,41 @@
+﻿import unittest
+from problem.p58_legnth_of_last_word import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = {
+            's': "Hello World"
+        }
+
+        output_data = {
+            'k': 5
+        }
+
+        self.assertEqual(output_data['k'], Solution().lengthOfLastWord(**input_data))
+
+    def test_example2(self):
+        input_data = {
+            's': "   fly me   to   the moon  "
+        }
+
+        output_data = {
+            'k': 4
+        }
+
+        self.assertEqual(output_data['k'], Solution().lengthOfLastWord(**input_data))
+
+    def test_example3(self):
+        input_data = {
+            's': "luffy is still joyboy"
+        }
+
+        output_data = {
+            'k': 6
+        }
+
+        self.assertEqual(output_data['k'], Solution().lengthOfLastWord(**input_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
