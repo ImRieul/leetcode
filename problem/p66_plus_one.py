18663fbc202547ed9414036505678600e1b62288
ImRieul <82865792+ImRieul@users.noreply.github.com>
2023-04-29 21:50:56 +0900

    230429

---
+++ problem/p66_plus_one.py
@@ -0,0 +1,9 @@
+﻿from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num_str: str = ''.join(list(map(str, digits)))
+        num: int = int(num_str) + 1
+
+        return list(map(int, str(num)))
---
+++ test/p66.py
@@ -0,0 +1,26 @@
+﻿import unittest
+from problem.p66_plus_one import *
+
+
+class TestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = [1, 2, 3]
+        expected = [1, 2, 4]
+
+        self.assertEqual(expected, Solution().plusOne(input_data))
+
+    def test_example2(self):
+        input_data = [4, 3, 2, 1]
+        expected = [4, 3, 2, 2]
+
+        self.assertEqual(expected, Solution().plusOne(input_data))
+
+    def test_example3(self):
+        input_data = [9]
+        expected = [1, 0]
+
+        self.assertEqual(expected, Solution().plusOne(input_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
