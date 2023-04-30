d39184132dd21aa7e4237db9b679b73042119b4b
ImRieul <82865792+ImRieul@users.noreply.github.com>
2023-04-30 11:04:46 +0900

    230430

---
+++ problem/p67_add_binary.py
@@ -0,0 +1,4 @@
+﻿class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        num_10 = int(a, 2) + int(b, 2)
+        return format(num_10, 'b')
---
+++ test/p67.py
@@ -0,0 +1,23 @@
+﻿import unittest
+from problem.p67_add_binary import *
+
+
+class TestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = ['11', '1']
+        expected = '100'
+        self.assertEqual(Solution().addBinary(*input_data), expected)
+
+    def test_example2(self):
+        input_data = ['1010', '1011']
+        expected = '10101'
+        self.assertEqual(Solution().addBinary(*input_data), expected)
+
+    def test_example3(self):
+        input_data = ['0', '0']
+        expected = '0'
+        self.assertEqual(Solution().addBinary(*input_data), expected)
+        
+
+if __name__ == '__main__':
+    unittest.main()
