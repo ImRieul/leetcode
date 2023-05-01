a4d5fbeb78dfcb0068d86fdc01c0813044f4dd84
ImRieul <82865792+ImRieul@users.noreply.github.com>
2023-05-02 07:51:03 +0900

    230502

---
+++ problem/p70_climbing_stairs.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def climbStairs(self, n: int) -> int:
+        fibonacci = [1, 1]
+
+        while (len(fibonacci) < n):
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+        return fibonacci[-1] + fibonacci[-2] if n > 1 else 1
---
+++ test/p70_test.py
@@ -0,0 +1,37 @@
+﻿import unittest
+from problem.p70_climbing_stairs import *
+
+
+class TestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = 2
+        expected = 2
+        self.assertEqual(Solution().climbStairs(input_data), expected)
+
+    def test_example2(self):
+        input_data = 3
+        expected = 3
+        self.assertEqual(Solution().climbStairs(input_data), expected)
+
+    def test_example3(self):
+        input_data = 4
+        expected = 5
+        self.assertEqual(Solution().climbStairs(input_data), expected)
+
+    def test_example4(self):
+        input_data = 5
+        expected = 8
+        self.assertEqual(Solution().climbStairs(input_data), expected)
+
+    def test_example5(self):
+        input_data = 1
+        expected = 1
+        self.assertEqual(Solution().climbStairs(input_data), expected)
+
+    def test_example6(self):
+        input_data = 6
+        expected = 13
+        self.assertEqual(Solution().climbStairs(input_data), expected)
+
+if __name__ == '__main__':
+    unittest.main()
