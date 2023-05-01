011566c6a67d4bf223eb7c10bcd5c3f412a4296c
ImRieul <82865792+ImRieul@users.noreply.github.com>
2023-05-01 19:58:05 +0900

    230501

---
+++ problem/p69_sqrt.py
@@ -0,0 +1,19 @@
+﻿# x(n+1) = (x(n) + a / x(n)) / 2
+#
+# 여기서 a는 제곱근을 구하려는 수이고,
+# x(0)은 임의의 초기값입니다.
+# 이 공식에서 x(n)은 n번째 반복에서의 근사값을 나타내며,
+# x(n+1)은 n+1번째 반복에서의 근사값을 나타냅니다.
+import math
+
+
+class Solution:
+    # def mySqrt(self, x: int) -> int:
+    def square_root(self, a):
+        default_num = 0.1
+
+        x1 = (default_num + a / default_num) / 2
+        while abs(x1 - default_num) > 1:
+            default_num = x1
+            x1 = (default_num + a / default_num) / 2
+        return math.floor(x1)
---
+++ test/p69_test.py
@@ -0,0 +1,27 @@
+﻿import unittest
+from problem.p69_sqrt import *
+
+
+class TestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = 4
+        expected = 2
+        self.assertEqual(expected, Solution().square_root(input_data))
+
+    def test_example2(self):
+        input_data = 8
+        expected = 2
+        self.assertEqual(expected, Solution().square_root(input_data))
+
+    def test_example3(self):
+        input_data = 9
+        expected = 3
+        self.assertEqual(expected, Solution().square_root(input_data))
+
+    def test_example4(self):
+        input_data = 3
+        expected = 1
+        self.assertEqual(expected, Solution().square_root(input_data))
+
+if __name__ == '__main__':
+    unittest.main()
