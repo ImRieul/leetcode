8d531e9715579cc4b5f7819c12b23e7605495aa7
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2023-05-07 10:22:22 +0900

    230507

---
+++ problem/p88_merge_sorted_array.py
@@ -0,0 +1,8 @@
+﻿from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for i in range(n):
+            nums1[-i-1] = nums2[i]
+        nums1.sort()
---
+++ test/p88_test.py
@@ -0,0 +1,48 @@
+﻿import unittest
+from problem.p88_merge_sorted_array import *
+
+class MyTestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = {
+            'nums1': [1, 2, 3, 0, 0, 0],
+            'm': 3,
+            'nums2': [2, 5, 6],
+            'n': 3
+        }
+
+        output_data = [1, 2, 2, 3, 5, 6]
+
+        Solution().merge(**input_data)
+        self.assertEqual(output_data, input_data.get('nums1'))
+
+
+    def test_example2(self):
+        input_data = {
+            'nums1': [1],
+            'm': 1,
+            'nums2': [],
+            'n': 0
+        }
+
+        output_data = [1]
+
+        Solution().merge(**input_data)
+        self.assertEqual(output_data, input_data.get('nums1'))
+
+    def test_example3(self):
+        input_data = {
+            'nums1': [0],
+            'm': 0,
+            'nums2': [1],
+            'n': 1
+        }
+
+        output_data = [1]
+
+        Solution().merge(**input_data)
+        self.assertEqual(output_data, input_data.get('nums1'))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
