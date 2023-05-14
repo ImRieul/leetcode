2d2c4ff0aafe8f47cff9a2fd8c5931fb6f630e47
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2023-05-14 23:22:31 +0900

    230514

---
+++ problem/p100_same_tree.py
@@ -0,0 +1,24 @@
+﻿from typing import Optional
+
+from library.node import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        check_list = self.inorder(p, q)
+        filter_list = list(filter(lambda x: x == False, check_list))
+
+        return len(filter_list) < 1
+
+    def inorder(self, p: Optional[TreeNode], q: Optional[TreeNode]):
+        check_list = []
+
+        if p is not None and q is not None:
+            check_list.extend(self.inorder(p.left, q.left))
+            check_list.append(p.val == q.val)
+            check_list.extend(self.inorder(p.right, q.right))
+        else:
+            check_list.append(p == q)
+
+        return check_list
---
+++ test/p100_test.py
@@ -0,0 +1,48 @@
+﻿import unittest
+from problem.p100_same_tree import *
+from library.node import TreeNode
+
+class MyTestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = {
+            'p': TreeNode(1, TreeNode(2), TreeNode(3)),
+            'q': TreeNode(1, TreeNode(2), TreeNode(3))
+        }
+
+        output_data = True
+
+        self.assertEqual(output_data, Solution().isSameTree(**input_data))
+
+    def test_example2(self):
+        input_data = {
+            'p': TreeNode(1, TreeNode(2)),
+            'q': TreeNode(1, None, TreeNode(2))
+        }
+
+        output_data = False
+
+        self.assertEqual(output_data, Solution().isSameTree(**input_data))
+
+    def test_example3(self):
+        input_data = {
+            'p': TreeNode(1, TreeNode(2), TreeNode(1)),
+            'q': TreeNode(1, TreeNode(1), TreeNode(2))
+        }
+
+        output_data = False
+
+        self.assertEqual(output_data, Solution().isSameTree(**input_data))
+
+    def test_example4(self):
+        input_data = {
+            'p': TreeNode(1, TreeNode(1)),
+            'q': TreeNode(1, None, TreeNode(1))
+        }
+
+        output_data = False
+
+        self.assertEqual(output_data, Solution().isSameTree(**input_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
