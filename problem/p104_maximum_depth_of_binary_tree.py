14f66b5df67d7f938631b29654c52e89860f173b
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2023-05-22 22:38:50 +0900

    230522

---
+++ problem/p104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,33 @@
+﻿from typing import Optional, List
+
+from library.node import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # depth_check = []
+        #
+        # depth: int = 0
+        #
+        # while root is not None:
+        #     depth += 1
+        #     if root.left is not None:
+        #         root = root.left
+        #     elif root.right is not None:
+        #         root = root.right
+        #     else:
+        #         depth_check.append(depth)
+        #         depth = 0
+
+        return self.deep(root, 0)
+
+
+    def deep(self, root: Optional[TreeNode], dept: int) -> int:
+        if root is not None:
+            dept += 1
+            left = self.deep(root.left, dept)
+            right = self.deep(root.right, dept)
+
+            dept = max(dept, left, right)
+
+        return dept
---
+++ test/p104_test.py
@@ -0,0 +1,26 @@
+﻿import unittest
+from problem.p104_maximum_depth_of_binary_tree import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_example1(self):
+        input_data = {
+            'root': TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+        }
+
+        output_data = 3
+
+        self.assertEqual(output_data, Solution().maxDepth(input_data['root']))
+
+    def test_example2(self):
+        input_data = {
+            'root': TreeNode(1, None, TreeNode(2))
+        }
+
+        output_data = 2
+
+        self.assertEqual(output_data, Solution().maxDepth(input_data['root']))
+
+
+if __name__ == '__main__':
+    unittest.main()
